5668f3c92919a7680978d62eae166181fead281f
inu-rei <ronaldramossierra@hotmail.com>
2024-07-21 15:43:15 -0500

    actualizado

--- src/components/Educacion.jsx
+++ src/components/Educacion.jsx
@@ -22,63 +22,69 @@ const Educacion = () => {
           <div className="diploma-image-container">
             <img src={diploma1} alt="Diploma Desarrollo Web" className="diploma-image" />
           </div>
-          <p>Diploma en Desarrollo Web Full Stack.</p>
+          <p>Diploma Desarrollo Front End Nivel Basico. 
+            30 horas de formación en Habilidades
+            Socioemocionales</p>
           <a href="/diplomas/diploma1.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma2} alt="Diploma Front-End" className="diploma-image" />
           </div>
-          <p>Diploma en Desarrollo Front-End.</p>
+          <p>Desarrollo Front End - Nivel Básico
+          con una duración de 125 horas.</p>
           <a href="/diplomas/diploma2.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma3} alt="Diploma Back-End" className="diploma-image" />
           </div>
-          <p>Diploma en Desarrollo Back-End.</p>
+          <p>Desarrollo Front End - Nivel Intermedio
+              formación en Habilidades
+              Socioemocionales con una duración de 30 horas.</p>
           <a href="/diplomas/diploma3.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma4} alt="Diploma Python" className="diploma-image" />
           </div>
-          <p>Diploma en Python.</p>
+          <p>Desarrollo Front End - Nivel Intermedio
+          con una duración de 125 horas..</p>
           <a href="/diplomas/diploma4.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma5} alt="Diploma Django" className="diploma-image" />
           </div>
-          <p>Diploma en Django.</p>
+          <p>HTML5 Y CSS3 PARTE 4:AVANZANDO EN CSS.</p>
           <a href="/diplomas/diploma5.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma6} alt="Diploma React" className="diploma-image" />
           </div>
-          <p>Diploma en React.</p>
+          <p>GIT Y GITHUB: CONTROLE Y COMPARTA SU CÓDIGO.</p>
           <a href="/diplomas/diploma6.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma7} alt="Diploma JavaScript" className="diploma-image" />
           </div>
-          <p>Diploma en JavaScript.</p>
+          <p>HTML5 Y CSS3 PARTE 3:TRABAJANDO CON FORMULARIOS Y TABLAS.</p>
           <a href="/diplomas/diploma7.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma8} alt="Diploma HTML/CSS" className="diploma-image" />
           </div>
-          <p>Diploma en HTML y CSS.</p>
+          <p>HTML5 Y CSS3 PARTE 2:POSICIONAMIENTO, LISTAS Y NAVEGACIÓN.</p>
           <a href="/diplomas/diploma8.pdf" download className="download-button">Descargar Diploma</a>
         </div>
         <div className="diploma-card">
           <div className="diploma-image-container">
             <img src={diploma9} alt="Diploma Full Stack" className="diploma-image" />
           </div>
-          <p>Diploma en Desarrollo Full Stack.</p>
+          <p>HTML5 Y CSS3 PARTE 1: MI PRIMERA PÁGINA WEB.</p>
           <a href="/diplomas/diploma9.pdf" download className="download-button">Descargar Diploma</a>
         </div>
       </div>
--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/Portafolio.css';
-import project1Image from '../assets/images/project1.png'; // Add your image
-import project2Image from '../assets/images/project2.png'; // Add your image
+import project1Image from '../assets/images/project1.png'; // Agrega tu imagen
+import project2Image from '../assets/images/project2.png'; // Agrega tu imagen
 import reactIcon from '../assets/icons/react-icon.png';
 import htmlIcon from '../assets/icons/html-icon.png';
 import cssIcon from '../assets/icons/css-icon.png';
@@ -16,7 +16,6 @@ const Portafolio = () => {
           <a href="https://12-caballeros-dorados.netlify.app" target="_blank" rel="noopener noreferrer">
             <img src={project1Image} alt="Proyecto 1" className="project-image" />
           </a>
-          <h3>Proyecto 1</h3>
           <a href="https://github.com/Inu-Rei/ProyectoUnidad7" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>
           <div className="tech-icons">
             <img src={reactIcon} alt="React" className="tech-icon" />
@@ -24,18 +23,30 @@ const Portafolio = () => {
             <img src={cssIcon} alt="CSS" className="tech-icon" />
             <img src={bootstrapIcon} alt="Bootstrap" className="tech-icon" />
           </div>
+          <div className="project-description">
+            <h3>Descripción de mi primer proyecto</h3>
+            <p>
+              Aplicación web en React y Bootstrap para gestionar y explorar personajes de una serie específica. Utiliza react-router-dom para el enrutamiento y hooks (useState, useEffect, useContext, useParams, useNavigate) para manejar el estado. Incluye un formulario básico con validación y redirección post-inicio de sesión. Muestra información detallada de personajes y usa parámetros de URL para cargar datos específicos, garantizando una navegación fluida y una experiencia interactiva.
+            </p>
+          </div>
         </div>
+
         <div className="project-card">
           <a href="https://proyecto-final-udistrital-pokemon.netlify.app" target="_blank" rel="noopener noreferrer">
             <img src={project2Image} alt="Proyecto 2" className="project-image" />
           </a>
-          <h3>Proyecto 2</h3>
-          <a href="https://github.com/Inu-Rei/ProyectoUnidad7" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>
+          <a href="https://github.com/Inu-Rei/PokedexReact.git" target="_blank" rel="noopener noreferrer">Ver en GitHub</a>
           <div className="tech-icons">
             <img src={reactIcon} alt="React" className="tech-icon" />
             <img src={htmlIcon} alt="HTML" className="tech-icon" />
             <img src={cssIcon} alt="CSS" className="tech-icon" />
             <img src={bootstrapIcon} alt="Bootstrap" className="tech-icon" />
+          </div>
+          <div className="project-description">
+            <h3>Descripción del segundo proyecto</h3>
+            <p>
+              Este proyecto es una aplicación web de gestión de Pokémon desarrollada con React y Bootstrap. Utiliza react-router-dom para el enrutamiento y varios hooks (useState, useEffect, useContext, useParams, useNavigate) para el manejo del estado y la navegación. Incluye un sistema de inicio de sesión con validación y redirección, así como rutas privadas protegidas que aseguran que solo los usuarios autenticados puedan acceder a ciertas páginas. La aplicación cuenta con un filtro de Pokémon por tipo mediante checkboxes y muestra una lista de Pokémon con detalles detallados y navegación dinámica.
+            </p>
           </div>
         </div>
       </div>
